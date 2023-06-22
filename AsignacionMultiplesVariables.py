995c4cab55426b383469c257cbc4258bd122abb8
Isaac Torres <isaac325z10@gmail.com>
2023-06-22 13:01:06 -0600

    este es el primer commit

---
+++ AsignacionMultiplesVariables.py
@@ -0,0 +1,9 @@
+# Python nos permite asignar valores a multiples variables 
+# Ejemplo
+
+name, lastName, age = 'Isaac' , 'Torres' , 28
+
+#print(name)
+#print(lastName)
+
+print (name, lastName, age)
